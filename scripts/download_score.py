df31eaf66e91b334022a253a027079bbdb9388b4
sebasgverde <sgarci18@eafit.edu.co>
2017-09-17 00:22:13 -0500

    Some of the files were protected so i added a log to know which files remains if I want the while datasets

--- scripts/download_score.py
+++ scripts/download_score.py
@@ -9,8 +9,11 @@ Date: April 2017
 import urllib.request
 import json
 import os
+import pickle
 
 def download_folder(folder_name):
+    general_string = ''
+
     if not os.path.exists(folder_name):
         # os.makedirs(folder_name)
         os.makedirs(folder_name + '/midi')
@@ -25,21 +28,30 @@ def download_folder(folder_name):
         # score_id = train_ids[0]
         print('downloading id:', score_id)
 
-        # First download score JSON to get secret
-        r = urllib.request.urlopen(score_json_url.format(score_id))
-        score_json = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
-        score_secret = score_json['secret']
+        try:
+            # First download score JSON to get secret
+            r = urllib.request.urlopen(score_json_url.format(score_id))
+            score_json = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
+            score_secret = score_json['secret']
 
-        # Define save location
-        filename = './' + folder_name + '/musicxml/{}.mxl'.format(score_id)
-        # Download score
-        urllib.request.urlretrieve(score_file_url.format(score_id, score_secret),
-                                   filename)
-        
-        filename = './' + folder_name + '/midi/{}.mid'.format(score_id)
-        # Download score
-        urllib.request.urlretrieve(score_file_midi_url.format(score_id, score_secret),
-                                   filename)
+            # Define save location
+            filename = './' + folder_name + '/musicxml/{}.mxl'.format(score_id)
+            # Download score
+            urllib.request.urlretrieve(score_file_url.format(score_id, score_secret),
+                                       filename)
+            
+            filename = './' + folder_name + '/midi/{}.mid'.format(score_id)
+            # Download score
+            urllib.request.urlretrieve(score_file_midi_url.format(score_id, score_secret),
+                                       filename)
+        except:
+            general_string += score_id + '\n'
+
+    # with open(folder_name+'.pickle', 'wb') as handle:
+    #     pickle.dump(general_string, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open(folder_name+'_protected.txt', 'w') as file:
+        file.write(general_string)
 
     print("Done!")    
 
